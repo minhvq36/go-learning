286b18265509b442bedf3e34646388b5ea5d7932
quocminh <minhvq36@gmail.com>
2026-03-11 00:27:04 +0700

    Add channel + context

--- cmd/goroutine/main.go
+++ cmd/goroutine/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func sayHello() {
-	fmt.Println("Hello from goroutine")
+func fetchData(ctx context.Context, ch chan<- int) {
+	defer close(ch)
+
+	data := 42
+
+	// mock time process
+	timer := time.NewTimer(1 * time.Second)
+	defer timer.Stop()
+
+	select {
+	case <-timer.C:
+		ch <- data
+	case <-ctx.Done():
+		return
+	}
 }
 
 func main() {
-	go sayHello()
-	fmt.Println("Hello from main")
-	time.Sleep(1 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	ch := make(chan int)
+
+	go fetchData(ctx, ch) // provide channel
+
+	select {
+	case val := <-ch:
+		fmt.Printf("Received: %d", val)
+	case <-ctx.Done():
+		fmt.Println("Cancelled")
+	}
+
 }
